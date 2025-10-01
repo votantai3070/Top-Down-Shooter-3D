0f11569c305aaed62940ad1ad6a04fe376eac37d
votantai3070 <votantai.07092002@gmail.com>
2025-10-02 02:02:09 +0700

    Setup Animation for Player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,12 +1,27 @@
+using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls controls;
 
-    public Vector2 moveInput;
-    public Vector2 aimInput;
+    private CharacterController characterController;
+
+    [Header("Movement Information")]
+    public float walkSpeed;
+    public Vector3 moveDirection;
+
+    [SerializeField] private float gravityScale = 9.81f;
+
+    private float verticalVelocity;
+
+    private Vector2 moveInput;
+    private Vector2 aimInput;
+
+    [Header("Aim Information")]
+    [SerializeField] Transform aim;
+    [SerializeField] LayerMask aimLayerMask;
+    private Vector3 lookingDirection;
 
     private void Awake()
     {
@@ -17,6 +32,55 @@ public class PlayerMovement : MonoBehaviour
 
         controls.Character.Aim.performed += ctx => aimInput = ctx.ReadValue<Vector2>();
         controls.Character.Aim.canceled += ctx => aimInput = Vector2.zero;
+    }
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+        AimTowardMouse();
+    }
+
+    private void AimTowardMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, aimLayerMask))
+        {
+            lookingDirection = hitInfo.point - transform.position;
+            lookingDirection.y = 0;
+            lookingDirection.Normalize();
+
+            transform.forward = lookingDirection;
+
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+        ApplyGravity();
+
+        if (moveDirection.magnitude > 0)
+        {
+            characterController.Move(Time.deltaTime * walkSpeed * moveDirection);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded == false)
+        {
+            verticalVelocity -= gravityScale * Time.deltaTime;
+            moveDirection.y = verticalVelocity;
+        }
+        else
+            verticalVelocity = -.5f;
     }
 
     private void OnEnable()
